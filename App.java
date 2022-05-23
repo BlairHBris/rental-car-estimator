64f3757a72c4a4a1d42d419aa844cd2aec4225ce
Blair Bristow <blairhbristow@gmail.com>
2022-05-23 18:33:20 -0400

    Better code

--- App.java
+++ App.java
@@ -1,89 +1,53 @@
 class App {
   public static void main(String args[]) {
-    App.carCalculation("5/22/2022", 3, true, false, true, 28);
+    App.carCombinator("5/22/2022", 3, true, false, true, 28);
   }
 
-  private static void carCalculation(String pickUpDate, float rentalDays, boolean tollTag, boolean gps, boolean roadsideAssistance, int age) {
-    float basicCharge = 29.99f * rentalDays;
-    double roundedBasicCharge = Math.round(basicCharge*100.0)/100.0;
-    System.out.println("The Basic car rental cost is " + roundedBasicCharge);
+  public static void carCombinator(String pickUpDate, float rentalDays, boolean tollTag, boolean gps, boolean roadsideAssistance, int age) {
+    double totalCost = basicRentalCost(rentalDays) + optionsCost(rentalDays, tollTag, gps, roadsideAssistance) + underageCost(rentalDays, age);
 
-    if (age < 25) {
-      if (tollTag && gps && roadsideAssistance) {
-        float optionCharge = rentalDays * 10.85f;
-        System.out.println("The total cost for all options is " + (rentalDays * 10.85));
-        float underageCharge = .3f * basicCharge;
-        System.out.println("The underage driver surcharge is " + underageCharge);
-        float totalCharge = underageCharge + basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (tollTag && gps) {
-        float optionCharge = rentalDays * 6.9f;
-        System.out.println("The total cost for all options is " + (rentalDays * 6.9));
-        float underageCharge = .3f * basicCharge;
-        System.out.println("The underage driver surcharge is " + underageCharge);
-        float totalCharge = underageCharge + basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (tollTag && roadsideAssistance) {
-        float optionCharge = rentalDays * 7.9f;
-        System.out.println("The total cost for all options is " + (rentalDays * 7.9));
-        float underageCharge = .3f * basicCharge;
-        System.out.println("The underage driver surcharge is " + underageCharge);
-        float totalCharge = underageCharge + basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (roadsideAssistance && gps) {
-        float optionCharge = rentalDays * 6.9f;
-        System.out.println("The total cost for all options is " + (rentalDays * 6.9));
-        float underageCharge = .3f * basicCharge;
-        System.out.println("The underage driver surcharge is " + underageCharge);
-        float totalCharge = underageCharge + basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (roadsideAssistance || tollTag) {
-        float optionCharge = rentalDays * 3.95f;
-        System.out.println("The total cost for all options is " + (rentalDays * 3.95));
-        float underageCharge = .3f * basicCharge;
-        System.out.println("The underage driver surcharge is " + underageCharge);
-        float totalCharge = underageCharge + basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (gps) {
-        float optionCharge = rentalDays * 2.95f;
-        System.out.println("The total cost for all options is " + (rentalDays * 2.95));
-        float underageCharge = .3f * basicCharge;
-        System.out.println("The underage driver surcharge is " + underageCharge);
-        float totalCharge = underageCharge + basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      }  
-    } else {
-      if (tollTag && gps && roadsideAssistance) {
-        float optionCharge = rentalDays * 10.85f;
-        System.out.println("The total cost for all options is " + (rentalDays * 10.85));
-        float totalCharge = basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (tollTag && gps) {
-        float optionCharge = rentalDays * 6.9f;
-        System.out.println("The total cost for all options is " + (rentalDays * 6.9));
-        float totalCharge = basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (tollTag && roadsideAssistance) {
-        float optionCharge = rentalDays * 7.9f;
-        System.out.println("The total cost for all options is " + (rentalDays * 7.9));
-        float totalCharge = basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (roadsideAssistance && gps) {
-        float optionCharge = rentalDays * 6.9f;
-        System.out.println("The total cost for all options is " + (rentalDays * 6.9));
-        float totalCharge = basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (roadsideAssistance || tollTag) {
-        float optionCharge = rentalDays * 3.95f;
-        System.out.println("The total cost for all options is " + (rentalDays * 3.95));
-        float totalCharge = basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-      } else if (gps) {
-        float optionCharge = rentalDays * 2.95f;
-        System.out.println("The total cost for all options is " + (rentalDays * 2.95));
-        float totalCharge = basicCharge + optionCharge;
-        System.out.println("The total cost is " + totalCharge);
-    }
+    System.out.println("The total cost is " + totalCost);
+  }
+
+private static double underageCost(float rentalDays, int age) {
+  if (age < 25) {
+    System.out.println("The Underage driver surcharge is " + rentalDays * 29.99 * 0.3 );
+    return rentalDays * 29.99 * 0.3;
+  } else {
+    System.out.println("There is no Underage driver surcharge");
+    return 0;
+  }
+  }
+
+
+private static double optionsCost(float rentalDays, boolean tollTag, boolean gps, boolean roadsideAssistance) {
+  if (tollTag && gps && roadsideAssistance) {
+    System.out.println("The Options cost is " + Math.round(rentalDays * 10.85 * 100.00)/100.00 );
+    return rentalDays * 10.85;
+  } else if (tollTag && gps) {
+    System.out.println("The Options cost is " + Math.round(rentalDays * 6.9 * 100.00)/100.00);
+    return rentalDays * 6.9;
+  } else if (tollTag && roadsideAssistance) {
+    System.out.println("The Options cost is " + Math.round(rentalDays * 7.9 * 100.00)/100.00 );
+    return rentalDays * 7.9;
+  }  else if (roadsideAssistance && gps) {
+    System.out.println("The Options cost is " + Math.round(rentalDays * 6.9 * 100.00)/100.00 );
+    return rentalDays * 6.9;
+  } else if (tollTag || roadsideAssistance) {
+    System.out.println("The Options cost is " + Math.round(rentalDays * 3.95 * 100.00)/100.00 );
+    return rentalDays * 3.95;
+  } else if (gps) {
+    System.out.println("The Options cost is " + Math.round(rentalDays * 2.95 * 100.00)/100.00 );
+    return rentalDays * 2.95;
+  } else {
+    System.out.println("There are no Options costs");
+    return 0;
   }
 }
+
+private static double basicRentalCost(float rentalDays) {
+  System.out.println("The Basic rental cost is " + Math.round(rentalDays * 29.99 * 100.00)/100.00 );
+  return rentalDays * 29.99;
+}
+
 }
